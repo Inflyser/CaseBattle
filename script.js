89973548817125bd721807013ad0c4f14a1e15da
Inflyser <89538138944s@gmail.com>
2025-05-17 15:11:45 +0300

    att7

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://t.me/GiftsCaseBattlebot/GiftsCaseBattle', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ telegram_id: Telegram.WebApp.initDataUnsafe.user.id })
+});
