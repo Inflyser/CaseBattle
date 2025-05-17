8b372363b962a5c707fa26c0d3433f9a4dc25c3d
Inflyser <89538138944s@gmail.com>
2025-05-17 15:21:27 +0300

    js script

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-fetch('https://t.me/GiftsCaseBattlebot/GiftsCaseBattle', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ telegram_id: Telegram.WebApp.initDataUnsafe.user.id })
-});
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+// fetch('https://t.me/GiftsCaseBattlebot/GiftsCaseBattle', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ telegram_id: Telegram.WebApp.initDataUnsafe.user.id })
+// });
