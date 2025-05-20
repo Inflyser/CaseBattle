f3cd9449b5d0730500731157d9155c25e28630b0
Inflyser <89538138944s@gmail.com>
2025-05-21 01:35:23 +0300

    front

---
+++ script/anim.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+  container: document.getElementById('animation-container'), // контейнер для анимации
+  renderer: 'svg', // рендеринг через SVG
+  loop: true,
+  autoplay: true,
+  path: '../Horror_Movie.json' // путь к распакованному JSON
+});
