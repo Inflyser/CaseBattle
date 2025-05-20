82a88778f356e7cb57b957db2d11ee4319c639f9
Inflyser <89538138944s@gmail.com>
2025-05-21 01:01:56 +0300

    front

--- script/button.js
+++ script/button.js
@@ -17,45 +17,45 @@ var infoType = getUrlParameter('info');
 
 if (infoType === 'box1') { 
     document.getElementById('name__case').textContent = 'Бесплатный за промокод';
-    document.getElementById('case__image').src = '../../assets/case/box1.png';
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 
 
 } else if (infoType === 'box2') {
     document.getElementById('name__case').textContent = 'Бесплатный за пополнение от 1000';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box3') {
     document.getElementById('name__case').textContent = 'Бесплатный за пополнение от 2000';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box4') {
     document.getElementById('name__case').textContent = 'Бесплатный за пополнение от 5000';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box5') {
     document.getElementById('name__case').textContent = 'Бесплатный за пополнение от 10000';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 
 } else if (infoType === 'box11') {
     document.getElementById('name__case').textContent = 'Бомж';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box12') {
     document.getElementById('name__case').textContent = 'Работяга';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box13') {
     document.getElementById('name__case').textContent = 'Офисный клерк';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box14') {
     document.getElementById('name__case').textContent = 'Директор';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 } else if (infoType === 'box15') {
     document.getElementById('name__case').textContent = 'Олигарх';
-
+    document.getElementById('case__image').src = `../../assets/case/${infoType}.png`;
 
 }
