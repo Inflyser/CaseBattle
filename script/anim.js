2edeccd3f7e6dea698ead1f8f09a027a2f6d37db
Inflyser <89538138944s@gmail.com>
2025-05-22 00:25:00 +0300

    front

--- script/anim.js
+++ script/anim.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-  lottie.loadAnimation({
+  // Загружаем анимацию, но не запускаем сразу
+  const animation = lottie.loadAnimation({
     container: document.getElementById('animation-container'),
     renderer: 'svg',
     loop: true,
-    autoplay: true,
+    autoplay: false,  // отключаем автозапуск
     path: '../../jsons/Horror_Movie.json'
+  });
+  // Кнопка запуска анимации
+  const playBtn = document.getElementById('play-btn');
+  playBtn.addEventListener('click', function () {
+    animation.play();  // запускаем анимацию по клику
   });
 });
