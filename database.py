f8fa4b1c190bdc8a944d7e7951bc4edb46fd21b4
Inflyser <89538138944s@gmail.com>
2025-05-22 23:58:15 +0300

    back+db

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(os.getenv("DATABASE_URL"))
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
--- init_db.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from models import Base
-from database import engine  # Импорты подгони под свою структуру
-
-async def init():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-asyncio.run(init())
